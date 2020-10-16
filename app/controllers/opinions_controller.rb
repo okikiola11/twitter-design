dabb15dca859d91eab5c4e739741e0dde7cbfa50
okikiola11 <okikiola.apelehin@gmail.com>
2020-10-16 11:21:20 +0000

    Add display of user opinion on the home page

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,22 +1,45 @@
 class OpinionsController < ApplicationController
+  before_action :set_opinion, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user
 
   def index
     @opinions = Opinion.all.order('created_at DESC')
+    @opinion = Opinion.new
   end
 
   def new
-    @opinion = current_user.opinions.new
+    @opinion = Opinion.new
   end
 
   def create
-    @opinion = current_user.opinions.create(author_id: current_user.id, Text: params[:Text])
+    @opinion = current_user.opinions.create(opinion_params)
     if @opinion.save
-      flash[:notice] = 'Your opinion has been saved successfully'
-      redirect_to root_path
+      flash[:notice] = 'Your opinion has been saved successfully'  
     else
       flash[:danger] = 'Could not save your opinion, Pleas try again!!!'
-      render :new
     end
+    redirect_to root_path
   end 
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def opinion_params
+    params.require(:opinion).permit(:author_id, :Text)
+  end
+
+  def set_opinion
+    @opinion = Opinion.find([params[:id]])
+  end
 end
