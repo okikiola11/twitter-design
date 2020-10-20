24dafe8f50e58788c62373be0a4f9455b67348af
okikiola11 <okikiola.apelehin@gmail.com>
2020-10-20 16:45:56 +0000

    Fix linting errors

--- spec/models/likes_spec.rb
+++ spec/models/likes_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-
   before :each do
     @user = User.create(id: 1, username: 'jason', fullname: 'Okikiola Smith', photo: nil, coverimage: nil)
-    @opinion = Opinion.create(id: 1, author_id: @user.id, Text: "eeeeeeeeeeeeeeeeee")
+    @opinion = Opinion.create(id: 1, author_id: @user.id, Text: 'eeeeeeeeeeeeeeeeee')
   end
 
   context 'Like Validations' do
@@ -21,5 +20,4 @@ RSpec.describe Like, type: :model do
     it { expect(described_class.reflect_on_association(:opinion).macro).to eq(:belongs_to) }
     it { expect(described_class.reflect_on_association(:user).macro).to eq(:belongs_to) }
   end
-  
 end
