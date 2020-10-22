3c4b90bbdabbf75feedc92d3356ba4264a0685bb
okikiola11 <okikiola.apelehin@gmail.com>
2020-10-22 21:50:25 +0000

    Edit the heroku configuration of the secret_key_base and master_key

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,13 +1,11 @@
-Cloudinary.config do |config|
-  config.cloud_name = ENV["cloud_name"]
-  config.api_key = ENV["API_KEY"]
-  config.api_secret = ENV["API_SECRET"]
-  config.secure = true
-  config.cdn_subdomain = true
-end
+# Cloudinary.config do |config|
+#   config.cloud_name = ENV["cloud_name"]
+#   config.api_key = ENV["API_KEY"]
+#   config.api_secret = ENV["API_SECRET"]
+#   config.secure = true
+#   config.cdn_subdomain = true
+# end
 
-# Cloudinary.cloud_name = 'cloud_name'
-# Cloudinary.api_key = 'ENV["API_KEY"]'
-# Cloudinary.api_secret = 'ENV["API_SECRET"]'
-# Cloudinary.secure = true
-# Cloudinary.cdn_subdomain = true
+Rails.application.credentials.cloudinary[:cloud_name]
+Rails.application.credentials.cloudinary[:api_key]
+Rails.application.credentials.cloudinary[:api_secret]
