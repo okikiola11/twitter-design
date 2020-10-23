c3d4953d38511ffa5087c64b57212cb711d4c338
okikiola11 <okikiola.apelehin@gmail.com>
2020-10-23 09:00:28 +0000

    Edit the env variables

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -9,3 +9,4 @@
 Rails.application.credentials.cloudinary[:cloud_name]
 Rails.application.credentials.cloudinary[:api_key]
 Rails.application.credentials.cloudinary[:api_secret]
+Rails.application.credentials.secret_key_base
