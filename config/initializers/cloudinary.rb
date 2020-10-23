d8141d7557fb3deab33c56c3b05ea116f7d8c28e
okikiola11 <okikiola.apelehin@gmail.com>
2020-10-23 13:11:43 +0000

    Fix heroku error Edit the application.rb file

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,11 +1,3 @@
-# Cloudinary.config do |config|
-#   config.cloud_name = ENV["cloud_name"]
-#   config.api_key = ENV["API_KEY"]
-#   config.api_secret = ENV["API_SECRET"]
-#   config.secure = true
-#   config.cdn_subdomain = true
-# end
-
 Rails.application.credentials.cloudinary[:cloud_name]
 Rails.application.credentials.cloudinary[:api_key]
 Rails.application.credentials.cloudinary[:api_secret]
