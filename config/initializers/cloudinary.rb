60eb08ee8da2eabad687e18c12ca0fd9bdf0d9ac
okikiola11 <okikiola.apelehin@gmail.com>
2020-10-27 13:10:27 +0000

    Add the procfile

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,6 @@
-Rails.application.credentials[Rails.env.to_sym][:cloudinary][:cloud_name]
-Rails.application.credentials[Rails.env.to_sym][:cloudinary][:api_key]
-Rails.application.credentials[Rails.env.to_sym][:cloudinary][:api_secret]
-Rails.application.credentials.secret_key_base
+# Cloudinary.config do |config|
+#   Rails.application.credentials[Rails.env.to_sym][:cloudinary][:cloud_name]
+#   Rails.application.credentials[Rails.env.to_sym][:cloudinary][:api_key]
+#   Rails.application.credentials[Rails.env.to_sym][:cloudinary][:api_secret]
+#   Rails.application.credentials.secret_key_base
+# end
